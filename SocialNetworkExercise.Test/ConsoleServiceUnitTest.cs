a3c5e831584bbe555261388520fa1aa6d563087a
Clara <clara230488@hotmail.com>
2017-07-09 15:16:31 +0100

    Decript Commands and Unit tests to method in charge to decript commands: ConvertMessageToCommand

---
+++ SocialNetworkExercise.Test/ConsoleServiceUnitTest.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoMoq;
+using SocialNetworkExercise.Services;
+using SocialNetworkExercise.Models;
+
+namespace SocialNetworkExercise.Test
+{
+    [TestClass]
+    public class ConsoleUnitTest
+    {
+        [TestMethod]
+        public void ConsoleServiceConvertMessageToCommand_Posting_ReturnsCommandUsernamePostingInfoMessage()
+        {
+            //Arrange
+            var mocker = new AutoMoqer();
+            string userName = "Alice";
+            string message = "I love the weather today";
+            string commandMessage = $"{userName} -> {message}";
+             
+            ICommandService commandService = new CommandService();
+            IDataService dataService = new DataService();
+            var ConsoleService = new ConsoleService(commandService, dataService);
+            string[] args = new string[0];
+
+            //Action
+            Command result = ConsoleService.ConvertMessageToCommand(commandMessage);
+
+            //Assert
+            Assert.AreEqual(result.CommandName, Enums.CommandEnum.Posting);
+            Assert.AreEqual(result.UserName, userName); 
+            Assert.AreEqual(result.Info, message);
+        }
+
+        [TestMethod]
+        public void ConsoleServiceConvertMessageToCommand_Follows_ReturnsCommandUsernameFollowingInfoUserToFollow()
+        {
+            //Arrange
+            var mocker = new AutoMoqer();
+            string userName = "Alice";
+            string userToFollow = "Bob";
+            string commandMessage = $"{userName} follows {userToFollow}";
+
+            ICommandService commandService = new CommandService();
+            IDataService dataService = new DataService();
+            var ConsoleService = new ConsoleService(commandService, dataService);
+            string[] args = new string[0];
+
+            //Action
+            Command result = ConsoleService.ConvertMessageToCommand(commandMessage);
+
+            //Assert
+            Assert.AreEqual(result.CommandName, Enums.CommandEnum.Follow); 
+            Assert.AreEqual(result.UserName, userName);
+            Assert.AreEqual(result.Info, userToFollow);
+        }
+
+        [TestMethod]
+        public void ConsoleServiceConvertMessageToCommand_Wall_ReturnsCommandUsernameWall()
+        {
+            //Arrange
+            var mocker = new AutoMoqer();
+            string userName = "Alice"; 
+            string commandMessage = $"{userName} wall   ";
+
+            ICommandService commandService = new CommandService();
+            IDataService dataService = new DataService();
+            var ConsoleService = new ConsoleService(commandService, dataService);
+            string[] args = new string[0];
+
+            //Action
+            Command result = ConsoleService.ConvertMessageToCommand(commandMessage);
+
+            //Assert
+            Assert.AreEqual(result.CommandName, Enums.CommandEnum.Wall);
+            Assert.AreEqual(result.UserName, userName); 
+        }
+
+        [TestMethod]
+        public void ConsoleServiceConvertMessageToCommand_Reading_ReturnsCommandUsernameReading()
+        {
+            //Arrange
+            var mocker = new AutoMoqer();
+            string userName = "Alice";
+            string commandMessage = $"{userName} ";
+
+            ICommandService commandService = new CommandService();
+            IDataService dataService = new DataService();
+            var ConsoleService = new ConsoleService(commandService, dataService);
+            string[] args = new string[0];
+
+            //Action
+            Command result = ConsoleService.ConvertMessageToCommand(commandMessage);
+
+            //Assert
+            Assert.AreEqual(result.CommandName, Enums.CommandEnum.Reading);
+            Assert.AreEqual(result.UserName, userName);
+        }
+
+        [TestMethod]
+        public void ConsoleServiceConvertMessageToCommand_Exit_ReturnsCommandExitNoUser()
+        {
+            //Arrange
+            var mocker = new AutoMoqer(); 
+            string commandMessage = $"exit ";
+
+            ICommandService commandService = new CommandService();
+            IDataService dataService = new DataService();
+            var ConsoleService = new ConsoleService(commandService, dataService);
+            string[] args = new string[0];
+
+            //Action
+            Command result = ConsoleService.ConvertMessageToCommand(commandMessage);
+
+            //Assert
+            Assert.AreEqual(result.CommandName, Enums.CommandEnum.Exit); 
+        }
+    }
+}
--- SocialNetworkExercise/Services/ConsoleService.cs
+++ SocialNetworkExercise/Services/ConsoleService.cs
@@ -3,12 +3,16 @@ using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
 using SocialNetworkExercise.Models;
 using SocialNetworkExercise.Enums;
+using System.Linq;
 
 namespace SocialNetworkExercise.Services
 {
     public class ConsoleService : IConsoleService
     {
-      
+        private const string KEYPOSTING = " -> ";
+        private const string KEYFOLLOW = " follows ";
+        private const string KEYWALL = " wall";  
+
         private readonly ICommandService _commandService;
         private readonly IDataService _dataService;
 
@@ -20,7 +24,58 @@ namespace SocialNetworkExercise.Services
 
         public Command ConvertMessageToCommand(string message)
         { 
-            return new Command() { UserName = "blablabla", CommandName = CommandEnum.Wall, Info = "blabla" };
+            Command command;
+            if (message.Contains(KEYPOSTING))
+            {
+                command = ReadCommand(message, KEYPOSTING, CommandEnum.Posting);
+            }
+            else if (message.Contains(KEYFOLLOW))
+            {
+                command = ReadCommand(message, KEYFOLLOW, CommandEnum.Follow); 
+            }
+            else if (message.Contains(KEYWALL))
+            {
+                command = ReadCommand(message, KEYWALL, CommandEnum.Wall);
+            }
+            else if (message.Trim().Equals(CommandEnum.Exit.ToString(), StringComparison.InvariantCultureIgnoreCase))
+            {
+                command = new Command();
+                command.CommandName = CommandEnum.Exit;
+            }
+            else
+            {
+                command = ReadCommand(message, null, CommandEnum.Reading); 
+            }
+            return command;
+        }
+
+        private Command ReadCommand(string message, string key, CommandEnum commandName)
+        {
+            Command command = new Command();
+
+            var messageSplit = key != null?
+                message.Split(new string[] { key }, StringSplitOptions.None) 
+                : new string[] { message };
+
+            var nElements = messageSplit.Length;
+
+            if (nElements >= 1)
+            {
+                command.CommandName = commandName;
+                command.UserName = messageSplit[0].Trim();
+
+                if (nElements >= 2)
+                { 
+                    var info = string.Join(key, messageSplit.Skip(1).Take(nElements - 1).ToArray()); 
+                    command.Info = info;
+                } 
+            }
+            else
+            {
+                command = null;
+            }
+
+            return command;
         }
 
         public string ExecuteCommand(Command command, Dictionary<string, User> data)
